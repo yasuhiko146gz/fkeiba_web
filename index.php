12c9740209eb49eb9039c3db80e81932903ee73f
yasuhiko146gz <yasuhiko@gnzo.com>
2025-08-07 20:41:33 +0900

    index_clean -> index, index -> yt

--- index.php
+++ index.php
@@ -44,11 +44,6 @@
   <!-- ICON -->
   <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 
-  <!-- resetcss -->
-  <!--
-  <link rel="stylesheet" type="text/css" href="https://yui.yahooapis.com/3.18.1/build/cssreset/cssreset-min.css">
-  -->
-
   <!-- Google tag (gtag.js) -->
   <script async src="https://www.googletagmanager.com/gtag/js?id=G-HRW2S9NW67"></script>
   <script>
@@ -66,18 +61,14 @@
 <!--
   <script src="./js/fvLivePlayer/lib/platform.js"></script>
   <script src="./js/fvLivePlayer/embed.min.js"></script>
--->
+--->
   <script src="https://tk3.s3.ap-northeast-1.amazonaws.com/fvLivePlayer/lib/platform.js"></script>
   <script src="https://tk3.s3.ap-northeast-1.amazonaws.com/fvLivePlayer/embed.min.js"></script>
 
-  <!-- YouTube Liveコメント表示CSS -->
-  <link href="./css/youtube-comments.css" rel="stylesheet" type="text/css">
   <!-- main style -->
   <link href="./css/style.css" rel="stylesheet" type="text/css">
   <!-- main js -->
   <script type="text/javascript" src="./js/main.js"> </script>
-  <!-- YouTube Liveコメント表示JS -->
-  <script type="text/javascript" src="./js/youtube-comments.js"> </script>
 
 </head>
 
@@ -106,23 +97,9 @@
       <ul></ul>
     </div>
 
-    <!-- メインコンテンツラッパー -->
-    <div class="main-content-wrapper">
-      <!-- プレーヤーとコメントのレスポンシブコンテナ -->
-      <div class="player-comments-container">
-        <!-- プレーヤーエリア -->
-        <div class="video-player-area">
-          <div class="video">
-            <div id='fvPlayer'></div>
-            <div id="cover"><span></span></div>
-          </div>
-        </div>
-        
-        <!-- YouTube Liveコメントエリア -->
-        <div class="youtube-comments-wrapper">
-          <div id="youtube-comments-container"></div>
-        </div>
-      </div>
+    <div class="video">
+      <div id='fvPlayer'></div>
+      <div id="cover"><span></span></div>
     </div>
   </div>
 
@@ -247,16 +224,10 @@
               <tbody>
                 <h3>注意事項</h3>
                 <tr>
-                  <!-- <th><p>注意事項</p></th> -->
                   <td>
                     <p>wifi環境など高速通信（下り3Mbps以上）が可能な電波の良い所でご視聴ください。</p>
                     <p>回線状況や電波状況の悪い環境下では、読み込みに時間がかかる場合があります。</p>
                     <p>モバイルWi-Fi、スマートフォンをご利用の際は、ご契約をされております通信会社のWebサイトに記載の注意事項をご確認の上ご利用ください。</p>
-                    <!-- <a href="https://www.nttdocomo.co.jp/info/safety/packet.html" target="_blank">NTT docomo</a><br>
-                    <a href="https://www.softbank.jp/mobile/support/procedure/charge_guide/trouble/confirm/" target="_blank">SoftBank</a><br>
-                    <a href="https://www.au.kddi.com/mobile/charge/reference/packet-caution/" target="_blank">au</a><br>
-                    <a href="https://www.uqwimax.jp/service/speedlimt_pop02.html" target="_blank">UQ WIMAX</a><br>
-                    ※上記以外のキャリアをご利用の方はご自身の契約内容をご確認のうえご視聴ください。 -->
                     <br>
                     <p>本配信は、スマートフォン、パソコン、タブレットのブラウザ上で無料視聴可能ですが、インターネット接続に関わる通信料は利用者様のご負担となります。</p>
                     <br>
@@ -372,7 +343,6 @@
         ],
 
         onReady: function(player) {
-          //console.log("HTML onReady");
           // Windowsでプレイヤーの表示が崩れるの対応
           setTimeout(function() {
             $(window).trigger("resize");
@@ -384,21 +354,6 @@
         }
       }
     );
-
-    function debugLog(message, limit = 100) {
-      const now = new Date();
-      const timeStr = now.toLocaleTimeString('ja-JP', {
-        hour12: false
-      }) + '.' + String(now.getMilliseconds()).padStart(3, '0');
-      const formattedMessage = `[${timeStr}] ${message}`;
-
-      $('#debug').append('<p>' + $('<div>').text(formattedMessage).html() + '</p>');
-
-      const lines = $('#debug p');
-      if (lines.length > limit) {
-        lines.first().remove();
-      }
-    }
 
     function getOrientation() {
       // Screen Orientation API available?
@@ -494,7 +449,6 @@
       // 前回と向きが変わった時のみ実施
       if (newOrientation !== _lastOrientation) {
         _lastOrientation = newOrientation;
-        //console.log('orientation changed → ' + newOrientation);
         if (isMobileDevice()) {
           if (getOrientation() == "landscape") {
             $('#header-container').hide();
@@ -508,9 +462,12 @@
             $('#ticker').show();
             $('.archive-notice').show();
             
-            // 縦向き時はiOS用CSSクラスを削除
+            // 縦向き時はiOS用CSSクラスとスタイルを削除
             if (isIOSDevice()) {
               document.body.classList.remove('ios-landscape');
+              document.body.style.height = '';
+              document.body.style.overflowY = '';
+              console.log('ios-landscapeクラスとスタイルを削除しました');
             }
           }
         }
@@ -522,12 +479,26 @@
      * iPhone Safari URLバー自動非表示処理
      */
     function handleUrlBarAutoHide() {
+      console.log('handleUrlBarAutoHide呼び出し:', {
+        isIOSDevice: isIOSDevice(),
+        isMobileLandScape: isMobileLandScape(),
+        orientation: getOrientation(),
+        userAgent: navigator.userAgent
+      });
+      
       if (!isIOSDevice() || !isMobileLandScape()) {
+        console.log('iOSまたは横向きではないため、URLバー非表示処理をスキップ');
         return;
       }
       
-      // iOS用CSSクラスを追加
+      // CSSクラスとJavaScriptで両方からスタイルを適用して確実にスクロール可能状態を作る
       document.body.classList.add('ios-landscape');
+      document.body.style.height = 'calc(100vh + 1px)';
+      document.body.style.overflowY = 'auto';
+      console.log('ios-landscapeクラスとスタイルを追加しました');
+      
+      // 即座に実行
+      hideUrlBarIfNeeded();
       
       // 少し遅延してからURLバー隠し処理を実行
       setTimeout(function() {
@@ -538,6 +509,11 @@
       setTimeout(function() {
         hideUrlBarIfNeeded();
       }, 400);
+      
+      // さらに遅延して実行
+      setTimeout(function() {
+        hideUrlBarIfNeeded();
+      }, 800);
     }
 
     /**
@@ -545,12 +521,23 @@
      */
     function hideUrlBarIfNeeded() {
       // URLバー表示状態をチェック
-      if (isUrlBarVisible()) {
+      var urlBarVisible = isUrlBarVisible();
+      var heightDiff = screen.width - window.innerHeight;
+      
+      console.log('hideUrlBarIfNeeded呼び出し:', {
+        urlBarVisible: urlBarVisible,
+        screenWidth: screen.width,
+        windowInnerHeight: window.innerHeight,
+        heightDiff: heightDiff,
+        threshold: 20
+      });
+      
+      if (urlBarVisible) {
         // 微小なスクロールでURLバーを隠す
         window.scrollTo(0, 1);
-        
-        // デバッグログ
-        console.log('URLバー非表示処理を実行しました');
+        console.log('window.scrollTo(0, 1)を実行しました');
+      } else {
+        console.log('URLバーはすでに非表示です');
       }
     }
 
@@ -605,40 +592,18 @@
       }
 
       if ($('#fvPlayer').is(':visible')) {
-        // レスポンシブレイアウト対応：プレーヤーエリアの幅を基準にする
-        var playerAreaWidth = $('.video-player-area').length > 0 ? $('.video-player-area').width() : windowWidth;
-        
-        // 1024px以上の場合は、コメント欄分の幅を考慮
-        if (windowWidth >= 1024) {
-          // 画面幅に応じてコメント欄幅を計算
-          var commentWidth;
-          if (windowWidth <= 1440) {
-            commentWidth = 300; // 1024px-1440px: 300px
-          } else {
-            commentWidth = 400; // 1441px以上: 400px
-          }
-          
-          // コメント欄 + gapを引く
-          var availableWidth = windowWidth - (commentWidth + 24);
-          // プレーヤーエリアの実際の幅と比較して小さい方を使用
-          availableWidth = Math.min(availableWidth, playerAreaWidth);
-        } else {
-          var availableWidth = playerAreaWidth;
-        }
-        
         new_width = (contentHeight - 47) * 16 / 9;
-        //console.log("contentHeight:" + contentHeight + " availableWidth:" + availableWidth + " new_width:" + new_width);
         
-        if (new_width < availableWidth) {
+        if (new_width < windowWidth) {
           // モバイル端末横向の場合、プレーヤー高さが端末高さと一致するようにする
           if (isMobileLandScape() && isIOSDevice()) {
             new_width = windowHeight * 16 / 9;
-            $('#fvPlayer').width(Math.min(new_width, availableWidth) + 'px');
+            $('#fvPlayer').width(new_width + 'px');
           } else {
             $('#fvPlayer').width(new_width + 'px');
           }
         } else {
-          $('#fvPlayer').width(availableWidth + 'px');
+          $('#fvPlayer').width(windowWidth + 'px');
         }
       }
       
@@ -657,24 +622,10 @@
       },
 
       updateRaceInfo: function(race_idx) {
-        // <li><a id="ref-info" href="https://www.keiba.go.jp/KeibaWeb_IPAT/TodayRaceInfo/DebaTable_ipat?k_raceDate=2024%2f02%2f08&k_raceNo=1&k_babaCode=19">馬柱</a></li>
-        // <li><a id="ref-odds" href="https://www.keiba.go.jp/KeibaWeb_IPAT/TodayRaceInfo/OddsTanFuku_ipat?k_raceDate=2024%2f02%2f07&k_raceNo=1&k_babaCode=19">オッズ</a></li>
-        // <li><a id="ref-pred" href="https://nar.sp.netkeiba.com/?pid=funabashi_shutuba&race_id=202443020701">予想</a>
-        //https://www.nankankeiba.com/uma_shosai/2024020919110501.do
-        /* 240208 正常動作確認済 → 澤田さん回答で没
-                race_idx_zeropad = race_idx.toString().padStart(2, '0');
-                info_url = "https://www.nankankeiba.com/uma_shosai/20240209191105"+race_idx_zeropad+".do";
-                $("#ref-info").attr("href",info_url);
-                odds_url = "https://nar.netkeiba.com/odds/index.html?race_id=2024430209"+race_idx_zeropad;
-                $("#ref-odds").attr("href",odds_url);
-                pred_url = "https://nar.sp.netkeiba.com/?pid=funabashi_shutuba&race_id=2024430209"+race_idx_zeropad;
-                $("#ref-pred").attr("href",pred_url);
-                console.log("updateRaceInfo race_idx="+race_idx+" info="+info_url+" odds="+odds_url+" pred="+pred_url);
-        */
+        // コメントアウトされた処理
       },
       updateCover: function(flg) {
         if (flg == 0) {
-          //console.log("cover hide");
           $("#cover").hide();
           $("#cover span").empty();
           $("#fvPlayer").show();
@@ -686,15 +637,11 @@
             ((ua.indexOf("win") != -1) && (ua.indexOf("trident") != -1)) ||
             ((ua.indexOf("win") != -1) && (ua.indexOf("edge") != -1))
           ) {
-            //console.log("updateCover flg=" + flg + " reload");
             location.reload();
           }
         } else {
-          //console.log("cover show content:" + cover_content[flg]["content"]);
           $("#cover span").html(cover_content[flg]["content"]);
           $("#cover").show();
-          //if (player_ref != null)
-          //  player_ref.pause();
           $("#fvPlayer").hide();
         }
         adjustCoverImageHeight();
